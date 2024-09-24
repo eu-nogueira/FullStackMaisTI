1cb7829b78004a6d4cd3cd15779f6e671fb47b74
eu-nogueira <marcosnogueirapro@gmail.com>
2024-09-24 20:19:27 -0300

    aula11

--- modulo3/aula7/ind.js
+++ modulo3/aula7/ind.js
@@ -1,18 +1,28 @@
 const newTar = document.querySelector('.newTar')
 const add = document.querySelector('.add')
 const list = document.querySelector('.list')
+const concluido = document.querySelector('.concluido')
+add.addEventListener('click', function () {
+    if (newTar.value) {
 
-add.addEventListener('click', function() {
-    if(newTar.value) {
-    let li = document.createElement('li')
-    li.textContent = newTar.value.trim() 
-    let btnRem = document.createElement('span')
-    btnRem.textContent = `remover`
-    btnRem.style.color = `Red`
-    list.appendChild(li)
-    li.appendChild(btnRem)
-    btnRem.addEventListener('click', function() {
-       li.remove()
-    })
-}
+        let li = document.createElement('li')
+        li.textContent = newTar.value.trim()
+        let btnRem = document.createElement('span')
+        btnRem.textContent = `remover`
+        btnRem.style.color = `Red`
+        list.appendChild(li)
+        li.appendChild(btnRem)
+        btnRem.style.paddingLeft = '10px'
+
+        newTar.value = ''
+        btnRem.addEventListener('click', function () {
+            li.remove()
+        })
+
+        concluido.addEventListener('click', function() {
+            let todas = []
+            todas.push(newTar.value)
+            li.replaceChild(li, todas)
+        })
+    }
 })
