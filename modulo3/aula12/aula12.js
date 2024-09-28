e908ae8d7125145517c7f9874404a16f44c71b8f
eu-nogueira <marcosnogueirapro@gmail.com>
2024-09-28 15:55:45 -0300

    Aula13

--- modulo3/aula12/aula12.js
+++ modulo3/aula12/aula12.js
@@ -5,13 +5,3 @@ let tipo = document.querySelector('.tipo')
 let desc = document.querySelector('.desc')
 let btnSave = document.querySelector('.btn')
 let cont = document.querySelector('.cont')
-
-btnSave.addEventListener('click', function (e) {
-    e.preventDefault()
-        if (Number(day.value) <= 31) {
-            let date = document.createElement('p')
-            date.textContent = `${day.value} ${month.value} ${year.value}`
-            date.style.marginTop = '10px'
-            cont.appendChild(date)
-        }
-})
