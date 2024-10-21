eeca8a6ae396999328af1c19ddd280531e0113cd
eu-nogueira <marcosnogueirapro@gmail.com>
2024-10-20 23:52:15 -0300

    exer4

--- modulo3/atividade4/new.js
+++ modulo3/atividade4/new.js
@@ -9,3 +9,24 @@ const swiper = new Swiper('.swiper', {
     }
 })
   AOS.init()
+let button = document.querySelector('.enviar')
+button.addEventListener('click', function() {
+
+    (function() {
+        emailjs.init("LKkPQT7lUQf4cdsLS")
+    })()
+
+    var params = {
+        sendername: document.querySelector('#sendername').value,
+        to: document.querySelector('#to').value,
+        message: document.querySelector('#message').value
+    }
+
+    var serviceID = "service_eobvmtr"
+    var templateID = "template_7zismvl"
+
+    emailjs.send(serviceID, templateID, params)
+    .then( res => {
+        alert `Email Enviado!`
+    })
+  })
