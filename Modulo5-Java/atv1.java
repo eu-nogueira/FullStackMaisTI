1695a0ade0b899fb1a74e824ce85a9808b63da48
eu-nogueira <marcosnogueirapro@gmail.com>
2025-01-06 22:09:17 -0300

    atv5

--- Modulo5-Java/atv1.java
+++ Modulo5-Java/atv1.java
@@ -2,6 +2,21 @@
 public class atv1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Digite o primeiro número: ");
+        int number1 = scanner.nextInt();
+        System.out.println("Digite o segundo número: ");
+        int number2 = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Digite o operador (+ - * ou /)");
+        String operador = scanner.nextLine();
+        if(operador.equals("+")) {
+            System.out.println(number1 + number2);
+        } else if (operador.equals("-")) {
+            System.out.println(number1 - number2);
+        } else if(operador.equals("*")) {
+            System.out.println(number1 * number2);
+        } else if(operador.equals("/")) {
+            System.out.println(number1 / number2);
+        }
     }
 }
